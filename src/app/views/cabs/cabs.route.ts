e95d68bf098580ef3f069747a8eca6bdcead080c
hatim <hatimhaiderali786@gmail.com>
2025-03-16 00:17:33 +0500

    add your car form added in car rental module

--- src/app/views/cabs/cabs.route.ts
+++ src/app/views/cabs/cabs.route.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component'
 import { ListComponent } from './list/list.component'
 import { DetailComponent } from './detail/detail.component'
 import { BookingComponent } from './booking/booking.component'
+import { CarRegistrationComponent } from './car-registration/car-registration.component'
 
 export const CABS_ROUTES: Route[] = [
   { path: 'home', component: HomeComponent, data: { title: 'Cab - Home' } },
@@ -10,11 +11,16 @@ export const CABS_ROUTES: Route[] = [
   {
     path: 'detail',
     component: DetailComponent,
-    data: { title: 'Cab - Detail' },
+    data: { title: 'Car - Detail' },
   },
   {
     path: 'booking',
     component: BookingComponent,
-    data: { title: 'Cab - Booking' },
+    data: { title: 'Car - Booking' },
   },
+  {
+    path: 'register-car',
+    component: CarRegistrationComponent,
+    data: {title: 'Car - Register'}
+  }
 ]
