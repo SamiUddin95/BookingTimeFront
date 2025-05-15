44e1889ca644401b244f5876911c0314908d43bf
Hatim <hatimhaiderali786@gmail.com>
2025-05-16 00:27:31 +0500

    update: car rental list sidebar filteration completed!

--- src/app/views/cabs/list/components/cablist/cablist.component.ts
+++ src/app/views/cabs/list/components/cablist/cablist.component.ts
@@ -5,13 +5,14 @@ import {
   HostListener,
   inject,
   Input,
+  OnChanges,
   OnInit,
   Renderer2,
+  SimpleChanges,
   type TemplateRef,
 } from '@angular/core'
 import { CablistfilterComponent } from '../cablistfilter/cablistfilter.component'
 import { CabalertComponent } from '../cabalert/cabalert.component'
-import { cabsLists } from '../../data'
 import {
   NgbOffcanvas,
   NgbOffcanvasModule,
@@ -20,10 +21,8 @@ import {
 } from '@ng-bootstrap/ng-bootstrap'
 import { RouterModule } from '@angular/router'
 import { StickyDirective } from '@/app/directives/sticky.directive'
-import { currency } from '@/app/store'
 import { CabCategoriesComponent } from "../cab-categories/cab-categories.component";
 import { CommonModule } from '@angular/common'
-import { CarRentalsService } from '@/app/core/services/api/car-rentals.service'
 import { NgxSliderModule, Options } from '@angular-slider/ngx-slider'
 import { CarResponse } from '@/app/core/models/cars/car.model'
 
@@ -49,41 +48,109 @@ import { CarResponse } from '@/app/core/models/cars/car.model'
     }
   `,
 })
-export class CablistComponent implements OnInit  {
+export class CablistComponent implements OnChanges {
 
-  //meta
-  value: number = 700
-  highValue: number = 1500
-  options: Options = {
-    floor: 500,
-    ceil: 2000,
-  }
+  sliderOptions!: Options;
 
-  @Input() carList: any = [];
-  filteredList: any = [];
+  filters = {
+    passengerSeats: new Set<number>(),
+    makes: new Set<number>(),
+    odometerReadings: new Set<number>(),
+    categories: new Set<number>(),
+    priceRange: {
+      min: 0,
+      max: 0
+    }
+  };
 
-  @Input() allCategories: any[] = []
-  selectedCategoryId: any
+  originalData!: CarResponse;
+  filteredData: any[] = [];
+  selectedCategoryId: number = 0;
 
   @Input() data!: CarResponse;
-
-  private carService = inject(CarRentalsService)
 
   public renderer = inject(Renderer2)
   public eleRef = inject(ElementRef)
   private offcanvasService = inject(NgbOffcanvas)
 
-  ngOnInit() {
-    this.filteredList = this.carList
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (changes['data'] && this.data) {
+      this.originalData = this.data;
+      this.filteredData = [...this.originalData.cars];
+    }
+
+    this.bindPriceRange();
+    this.clearAllFilters();
   }
 
   openFilter(content: TemplateRef<any>) {
     this.offcanvasService.open(content, { position: 'end' })
   }
 
-  selectCategory(categoryId: number) {
-    this.selectedCategoryId = categoryId;
-    this.filteredList = this.carList.filter((car: { categoryId: number }) => car.categoryId === categoryId);
+  selectCategory(id: number): void {
+
+    if (this.selectedCategoryId === id) {
+      this.selectedCategoryId = 0;
+      this.filters.categories.clear();
+    } else {
+
+      this.selectedCategoryId = id;
+      this.filters.categories.clear();
+      this.filters.categories.add(id);
+    }
+    this.applyFilters();
+  }
+
+
+  bindPriceRange() {
+    this.filters.priceRange.min = this.data.priceRange.lowestPrice;
+    this.filters.priceRange.max = this.data.priceRange.highestPrice;
+
+    this.sliderOptions = {
+      floor: this.filters.priceRange.min,
+      ceil: this.filters.priceRange.max,
+      step: 10
+    };
+  }
+
+  onCheckboxChange(filterKey: keyof typeof this.filters, value: any, event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+    const set = this.filters[filterKey] as Set<any>;
+
+    if (checkbox.checked) {
+      set.add(value);
+    } else {
+      set.delete(value);
+    }
+
+    this.applyFilters();
+  }
+
+  applyFilters(): void {
+    const { passengerSeats, makes, odometerReadings, priceRange, categories } = this.filters;
+
+    this.filteredData = this.originalData.cars.filter((car: any) => {
+      const matchSeats = passengerSeats.size === 0 || passengerSeats.has(car.passengerCapacity);
+      const matchMakes = makes.size === 0 || makes.has(car.makeId);
+      const matchOdometer = odometerReadings.size === 0 || odometerReadings.has(car.odometerId);
+      const matchPrice = car.totalRent >= priceRange.min && car.totalRent <= priceRange.max;
+      const matchCategory = categories.size === 0 || categories.has(car.categoryId);
+
+      return matchSeats && matchMakes && matchOdometer && matchPrice && matchCategory;
+    });
+  }
+
+  clearAllFilters() {
+    this.filters.passengerSeats.clear();
+    this.filters.makes.clear();
+    this.filters.odometerReadings.clear();
+    this.filters.categories.clear();
+    this.selectedCategoryId = 0;
+    this.filters.priceRange.min = this.sliderOptions.floor!;
+    this.filters.priceRange.max = this.sliderOptions.ceil!;
+
+    this.applyFilters();
   }
 
 }
